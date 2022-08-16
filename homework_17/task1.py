02377d4e33ded58e563895da70bfd3d80c7f845d
vadympozniak <vadym.pozniak@gmail.com>
2022-08-16 17:02:22 +0300

    python

---
+++ homework_17/task1.py
@@ -0,0 +1,41 @@
+"""Task 1
+Method overloading.
+Create a base class named Animal with a method called talk and then create two subclasses:
+Dog and Cat, and make their own implementation of the method talk be different.
+For instance, Dog’s can be to print ‘woof woof’, while Cat’s can be to print ‘meow’.
+Also, create a simple generic function, which takes as input instance of a Cat or Dog classes
+and performs talk method on input parameter.  """
+
+
+class Animal:
+    def __init__(self):
+        pass
+
+    def talk(self):
+        raise NotImplementedError('Must be implemented by a sub class')
+
+
+class Cat(Animal):
+    def talk(self):
+        print('Meow-meow!')
+
+
+class Dog(Animal):
+    def talk(self):
+        print('Woof-woof!')
+
+
+class YourDogTalk(Dog):
+    def talk(self):
+        sound = input('Enter your animal sound: ')
+        print(f'Your animal sound: {sound}')
+
+
+my_cat = Cat()
+my_cat.talk()
+
+my_dog = Dog()
+my_dog.talk()
+
+my_dog = YourDogTalk()
+my_dog.talk()
---
+++ homework_17/task2.py
@@ -0,0 +1,116 @@
+"""Library
+Write a class structure that implements a library. Classes:
+1) Library - name, books = [], authors = []
+2) Book - name, year, author (author must be an instance of Author class)
+3) Author - name, country, birthday, books = []
+Library class
+Methods:
+- new_book(name: str, year: int, author: Author) - returns an instance of Book
+class and adds the book to the books list for the current library.
+- group_by_author(author: Author) - returns a list of all books grouped by the specified author
+- group_by_year(year: int) - returns a list of all the books grouped by the specified year
+All 3 classes must have a readable __repr__ and __str__ methods."""
+
+
+class Author:
+    author_total_numbers = 0
+
+    def __init__(self, name, country, birthday, books):
+        self.name = name
+        self.country = country
+        self.birthday = birthday
+        self.books = books
+        Author.author_total_numbers += 1
+
+    def __str__(self):
+        return f'Full name: {self.name}. \n' \
+               f'Country: {self.country}. \n' \
+               f'Birthday: {self.birthday}. \n' \
+               f'Books: {self.books}.\n'
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class Book(Author):
+    book_total_numbers = 0
+
+    def __init__(self, name, year, author):
+        self.name = name
+        self.year = year
+        self.author = author
+        Book.book_total_numbers += 1
+
+    def __str__(self):
+        return f'Book name: «{self.name}». \n' \
+               f'Year: {self.year}. \n' \
+               f'Author: {self.author.name} / {self.author.country} / {self.author.birthday}.\n'
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class Library(Book):
+    def __init__(self, name):
+        self.name = name
+        self.all_books = []
+
+    def new_book(self, book):
+        if str(book.year).isdigit():
+            pass
+        else:
+            self.all_books.append({'name': book.name, 'year': book.year, 'author': book.author.name})
+
+    def group_by_author(self, author):
+        book_count = 0
+        print(f'All «{author.name}» books: ')
+        for book in self.all_books:
+            if author.name == book['author']:
+                book_count += 1
+                print(f"№{book_count}: «{book['name']}» ({book['year']} year)")
+        print()
+        if book_count == 0:
+            print(f'Books «{author.name}» {self.name} not found.\n')
+
+    def group_by_year(self, year):
+        book_count = 0
+        print(f'All {year} year books: ')
+        for book in self.all_books:
+            if year == book['year']:
+                book_count += 1
+                print(f"№{book_count}: «{book['name']}», author— {book['author']}")
+        print()
+        if book_count == 0:
+            print(f'{self.name} does not have books {year} year.\n')
+
+    def __str__(self):
+        return f'Welcome to {self.name}! \n' \
+               f'We have {Book.book_total_numbers} books.\n' \
+               f'By {Author.author_total_numbers} authors.\n'
+
+    def __repr__(self):
+        return self.__str__()
+
+
+our_library = Library('Library')
+
+author_1 = Author('Serhiy Zhadan', 'Ukraine', '23.08.1974', [])
+author_2 = Author('Yuval Noah Harari', 'Israel', '24.02.1976', [])
+
+book_1 = Book('Месопотамія', '2014', author_1)
+book_2 = Book('Тамплієри', '2016', author_1)
+book_3 = Book('Sapiens: A Brief History of Humankind', '2014', author_2)
+book_4 = Book('Homo Deus: A Brief History of Tomorrow ', '2016', author_2)
+
+our_library.new_book(book_1)
+our_library.new_book(book_2)
+our_library.new_book(book_3)
+our_library.new_book(book_4)
+
+our_library.group_by_author(author_1)
+our_library.group_by_author(author_2)
+
+our_library.group_by_year('2014')
+our_library.group_by_year('2016')
+
+print(our_library.__str__())
